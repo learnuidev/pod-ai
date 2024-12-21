aed5c6ac8b337d8cd671fa88fdaa58ca9d571144
learnuidev <learnuidev@gmail.com>
2024-12-21 01:13:16 -0500

    change to pod

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,7 @@ function FancyButton({ children }: { children: React.ReactNode }) {
 export default function Home() {
   return (
     <div>
-      <h1 className="text-center mt-32 text-8xl font-extrabold">
-        pod<span className="text-gray-500">.ai</span>
-      </h1>
+      <h1 className="text-center mt-32 text-8xl font-extrabold">pod</h1>
 
       <p className="text-center text-gray-400 text-2xl mt-2 font-extralight">
         turn anything into a podcast
